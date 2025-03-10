a04d84e25ec707185d75f54cfc55c50318312313
alfitranurr <alfitranurr@gmail.com>
2025-03-10 22:04:01 +0700

    update Certificate DataBase

--- frontend/src/database/Certificate/License-Certification.tsx
+++ frontend/src/database/Certificate/License-Certification.tsx
@@ -1,4 +1,13 @@
-import OrganizationImage from "../../assets/images/Pantai.png";
+import Quran30Juz_Surkati from "../../assets/images/Certificate/LicenseCertification/Certificate - Memorizing the Quran 30 Juz in One Sitting.png";
+import SLQ_UMM from "../../assets/images/Certificate/LicenseCertification/Certificate - Semarak Literasi Al-Quran - 2023.png";
+import MySkill_DataAnalysis from "../../assets/images/Certificate/LicenseCertification/Certificate - Data Analysis Fullstack Intensive Bootcamp.png";
+import MySkill_MarketingManagement from "../../assets/images/Certificate/LicenseCertification/Certificate - E-Learning MySkill Marketing Management.png";
+import ASEAN_DSE from "../../assets/images/Certificate/LicenseCertification/Certificate -  ASEAN DSE - 2024.png";
+import OracleAcademy from "../../assets/images/Certificate/LicenseCertification/Certificate - Oracle Academy_Database - 2024.png";
+import DicodingAcademy from "../../assets/images/Certificate/LicenseCertification/Certificate - Dicoding Academy_Belajar Dasar Data Science - 2023.png";
+import FLSPUMM from "../../assets/images/Certificate/LicenseCertification/Certificate - Qualification of FLSP UMM - 2024.png";
+import ExcelEdutify from "../../assets/images/Certificate/LicenseCertification/Certificate - Filosofi Excel by Edutify - 2024.png";
+import ExcelKarirNext from "../../assets/images/Certificate/LicenseCertification/Certificate - Karirnex Bootcamp Excel - 2024.png";
 
 interface License {
   id: number;
@@ -15,57 +24,141 @@ interface License {
 const licenses: License[] = [
   {
     id: 1,
-    title: "Certified Ethical Hacker (CEH)",
+    title: "MySkill - Data Analysis Fullstack Intensive Bootcamp",
     description:
       "A certification that focuses on ethical hacking skills, including penetration testing, network security, and risk management, ideal for professionals seeking careers in cybersecurity.",
-    image: OrganizationImage,
-    tags: ["Certification", "Cybersecurity", "Ethical Hacking", "IT"],
+    image: MySkill_DataAnalysis,
+    tags: ["National"],
     buttons: [
       {
         text: "View Certificate",
-        link: "/licenses/ethical-hacker", 
+        link: "https://drive.google.com/drive/folders/1UIaqEc2T4qEtJ5myMbDjtlSbaqiZYCZu?usp=sharing",
       },
     ],
   },
   {
     id: 2,
-    title: "Project Management Professional (PMP)",
+    title: "MySkill - Marketing Management",
     description:
-      "A globally recognized certification for project management professionals that demonstrates your skills and expertise in leading projects and teams.",
-    image: OrganizationImage,
-    tags: ["Certification", "Project Management", "Leadership", "Business"],
+      "A certification that focuses on ethical hacking skills, including penetration testing, network security, and risk management, ideal for professionals seeking careers in cybersecurity.",
+    image: MySkill_MarketingManagement,
+    tags: ["National"],
     buttons: [
       {
         text: "View Certificate",
-        link: "/licenses/project-management-professional", 
+        link: "https://drive.google.com/file/d/1NTAvHDFl-3lpn2GyAxbDMipeDwZpaKQ8/view?usp=sharing",
       },
     ],
   },
   {
     id: 3,
-    title: "Google Analytics Certification",
+    title: "UMM - Semarak Literasi Al-Quran",
     description:
-      "A certification program for mastering Google Analytics, enabling you to track and analyze website traffic, understand audience behavior, and improve online marketing strategies.",
-    image: OrganizationImage,
-    tags: ["Certification", "Analytics", "Digital Marketing", "Google"],
+      "A globally recognized certification for project management professionals that demonstrates your skills and expertise in leading projects and teams.",
+    image: SLQ_UMM,
+    tags: ["Local"],
     buttons: [
       {
         text: "View Certificate",
-        link: "/licenses/google-analytics",
+        link: "https://drive.google.com/file/d/1S43dnSJKZAu6LCRad_PHCKS84rfDWP0w/view?usp=sharing",
       },
     ],
   },
   {
     id: 4,
-    title: "Certified Public Accountant (CPA)",
+    title: "As-Surkati - Memorizing the Quran 30 Juz in One Sitting",
     description:
-      "A certification for accounting professionals that is recognized worldwide, demonstrating expertise in accounting principles, taxation, and financial reporting.",
-    image: OrganizationImage,
-    tags: ["Certification", "Accounting", "Finance", "Taxation"],
+      "A certification program for mastering Google Analytics, enabling you to track and analyze website traffic, understand audience behavior, and improve online marketing strategies.",
+    image: Quran30Juz_Surkati,
+    tags: ["Local"],
     buttons: [
       {
         text: "View Certificate",
-        link: "/licenses/public-accountant", 
+        link: "https://drive.google.com/drive/folders/1SKS3Ntr0Bmr96ojVh7WwPetOZTj2sKTO?usp=sharing",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "ASEAN Data Science Explorers 2024 Enablement Session",
+    description:
+      "A certification for accounting professionals that is recognized worldwide, demonstrating expertise in accounting principles, taxation, and financial reporting.",
+    image: ASEAN_DSE,
+    tags: ["National"],
+    buttons: [
+      {
+        text: "View Certificate",
+        link: "https://drive.google.com/file/d/1ZP4BagM1dobaerWz_Quc1j-Y_h-e9K_q/view?usp=sharing",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Oracle Academy - Database",
+    description:
+      "A certification for accounting professionals that is recognized worldwide, demonstrating expertise in accounting principles, taxation, and financial reporting.",
+    image: OracleAcademy,
+    tags: ["National"],
+    buttons: [
+      {
+        text: "View Certificate",
+        link: "https://drive.google.com/file/d/1M_HxiW0cpfp2bU8DkDcLWzu0LtECZZo_/view?usp=sharing",
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "Dicoding Academy - Data Science",
+    description:
+      "A certification for accounting professionals that is recognized worldwide, demonstrating expertise in accounting principles, taxation, and financial reporting.",
+    image: DicodingAcademy,
+    tags: ["National"],
+    buttons: [
+      {
+        text: "View Certificate",
+        link: "https://drive.google.com/drive/folders/1oocyFFkiWcciPSuKpd8mCTUPmngUgVR3?usp=sharing",
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: "UMM - FLSP",
+    description:
+      "A certification for accounting professionals that is recognized worldwide, demonstrating expertise in accounting principles, taxation, and financial reporting.",
+    image: FLSPUMM,
+    tags: ["National"],
+    buttons: [
+      {
+        text: "View Certificate",
+        link: "https://drive.google.com/file/d/1IhaILHUtunljwD1AGQ_5lCGYIXnq2BcS/view?usp=sharing",
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: "Edutify - Filosofi Excel",
+    description:
+      "A certification for accounting professionals that is recognized worldwide, demonstrating expertise in accounting principles, taxation, and financial reporting.",
+    image: ExcelEdutify,
+    tags: ["National"],
+    buttons: [
+      {
+        text: "View Certificate",
+        link: "https://drive.google.com/file/d/1aTzpzon0C8Ny4mwVyFC1uRtDfNNyoWXk/view?usp=sharing",
+      },
+    ],
+  },
+  {
+    id: 10,
+    title: "KarirNext - Karirnex Bootcamp Excel",
+    description:
+      "A certification for accounting professionals that is recognized worldwide, demonstrating expertise in accounting principles, taxation, and financial reporting.",
+    image: ExcelKarirNext,
+    tags: ["National"],
+    buttons: [
+      {
+        text: "View Certificate",
+        link: "https://drive.google.com/file/d/1c1PPOV_jvyBACZfRPAZDTXAmGelyLNOb/view?usp=sharing",
       },
     ],
   },
