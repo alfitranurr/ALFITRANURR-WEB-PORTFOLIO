de0f6e479a40931933f5f61caaf4628f7bd9a27a
alfitranurr <alfitranurr@gmail.com>
2025-03-25 10:39:57 +0700

    Data Analytics

--- frontend/src/database/Projects/Data/dataanalytic.tsx
+++ frontend/src/database/Projects/Data/dataanalytic.tsx
@@ -1,81 +1,17 @@
-import OrganizationImage from "../../../assets/images/Pantai.png";
+import Danantara from "../../../assets/images/Projects/Data/DataAnalytic/Danantara.png";
 
 export const dataanalytic = [
   {
     id: 1,
-    title: "Data Analytics Research Group",
-    description:
-      "A global group dedicated to advancing research in data analytics and machine learning, promoting innovation and collaboration among data science experts.",
-    image: OrganizationImage,
-    tags: ["Committee", "Data Analytics", "Research"],
+    title:
+      "Sentiment Analysis of Indonesian Citizens' YouTube Comments on Danantara",
+    description: "",
+    image: Danantara,
+    tags: ["Youtube"],
     buttons: [
       {
-        text: "View Committee",
-        link: "https://drive.google.com/drive/folders/1-4R1VSKn6FQnBoxEx4ZwyvFTyP970WQj?usp=sharing", // Adjust the link accordingly
-      },
-      {
         text: "View Project",
-        link: "/projects/data-analytics-research-group", // Adjust the link accordingly
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Data for Good Coalition",
-    description:
-      "A coalition of organizations focused on using data to solve global challenges such as climate change, healthcare, and education.",
-    image: OrganizationImage,
-    tags: ["Committee", "Data Analytics", "Social Impact"],
-    buttons: [
-      {
-        text: "View Committee",
-        link: "/committee/data-for-good", // Adjust the link accordingly
-      },
-      {
-        text: "View Project",
-        link: "/projects/data-for-good-coalition", // Adjust the link accordingly
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "Data Ethics and Governance Forum",
-    description:
-      "An interdisciplinary forum focusing on the ethical implications of data use, governance, and regulatory frameworks.",
-    image: OrganizationImage,
-    tags: ["Committee", "Data Ethics", "Governance"],
-    buttons: [
-      {
-        text: "View Committee",
-        link: "/committee/data-ethics-governance", // Adjust the link accordingly
-      },
-      {
-        text: "View Project",
-        link: "/projects/data-ethics-governance-forum", // Adjust the link accordingly
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: "Data Startups and Innovation Hub",
-    description:
-      "An initiative to foster innovation in the data startup ecosystem, connecting entrepreneurs, investors, and professionals in the data science field.",
-    image: OrganizationImage,
-    tags: [
-      "Committee",
-      "Data Startups",
-      "Innovation",
-      "Entrepreneurship",
-      "Data",
-    ],
-    buttons: [
-      {
-        text: "View Committee",
-        link: "/committee/data-startups-innovation", // Adjust the link accordingly
-      },
-      {
-        text: "View Project",
-        link: "/projects/data-startups-innovation-hub", // Adjust the link accordingly
+        link: "https://github.com/alfitranurr/DATA-INFORMATION-KNOWLEDGE/tree/main/Youtube/Danantara",
       },
     ],
   },
