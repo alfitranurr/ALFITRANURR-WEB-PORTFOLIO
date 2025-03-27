515bee3d9931372674274dd79fca0f4476aa7825
alfitranurr <alfitranurr@gmail.com>
2025-03-27 09:58:23 +0700

    update google play store

--- frontend/src/database/Projects/Data/dataanalytic.tsx
+++ frontend/src/database/Projects/Data/dataanalytic.tsx
@@ -1,8 +1,22 @@
 import Danantara from "../../../assets/images/Projects/Data/DataAnalytic/Danantara.png";
+import GooglePlayStore from "../../../assets/images/Projects/Data/DataAnalytic/Google Play Store.jpg";
 
 export const dataanalytic = [
   {
     id: 1,
+    title: "Google Play Store",
+    description: "",
+    image: GooglePlayStore,
+    tags: ["Google"],
+    buttons: [
+      {
+        text: "View Project",
+        link: "https://github.com/alfitranurr/DATA-INFORMATION-KNOWLEDGE/tree/main/GooglePlayStore",
+      },
+    ],
+  },
+  {
+    id: 2,
     title:
       "Sentiment Analysis of Indonesian Citizens' YouTube Comments on Danantara",
     description: "",
