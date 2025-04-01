b7c9c10f9a33c879d6b9fee3971c3bdc7ec50584
alfitranurr <alfitranurr@gmail.com>
2025-04-01 17:32:08 +0700

    Kimia Farma BDA Rakamin Academy

--- frontend/src/database/Projects/Data/datavisualization.tsx
+++ frontend/src/database/Projects/Data/datavisualization.tsx
@@ -9,12 +9,26 @@ import SIC_INDICATION from "../../../assets/images/Projects/Data/DataVisualizati
   /* DASHBOARD */
 }
 import Tokopedei from "../../../assets/images/Projects/Data/DataVisualization/Dashboard - Tokopedei Transaction Dashboard.png";
+import RakaminKimiaFarmaBDA from "../../../assets/images/Projects/Data/DataVisualization/Dashboard - Big Data Analytics Kimia Farma.png";
 
 export const datavisualization = [
   {
     id: 1,
+    title: "Rakamin Academy x Kimia Farma - Performance Analytics 2020 - 2023",
+    description: "",
+    image: RakaminKimiaFarmaBDA,
+    tags: ["Dashboard"],
+    buttons: [
+      {
+        text: "View Dashboard",
+        link: "https://lookerstudio.google.com/embed/u/0/reporting/d65c0d29-cc49-474b-adc0-538a16233d4e/page/pkBFF",
+      },
+    ],
+  },
+  {
+    id: 2,
     title:
-      "Infographic on Telecommunication Resilience in Indonesia as a Catalyst for a Golden Indonesia 2045",
+      "4C UB - Infographic on Telecommunication Resilience in Indonesia as a Catalyst for a Golden Indonesia 2045",
     description:
       "This project is an infographic work created in an infographic competition held by 4C Universitas Brawijaya to enliven the anniversary event of the Faculty of Computer Science. This work discusses how strengthening telecommunication resilience can prevent internet access gaps in Indonesia, in order to ensure the achievement of the Indonesian Golden Society 5.0 by 2045.",
     image: UB_4C,
@@ -31,8 +45,8 @@ export const datavisualization = [
     ],
   },
   {
-    id: 2,
-    title: "ANAVA#19 FMIPA UGM Infographic on Sexual Violence in Indonesia",
+    id: 3,
+    title: "ANAVA#19 FMIPA UGM - Infographic on Sexual Violence in Indonesia",
     description:
       "This project is an infographic work created in the Statistics Infographics (KIS) competition which is held by ANAVA #19 Gadjah Mada University every year. This work explains the portrait of sexual violence in Indonesia, including data on the distribution of cases, perpetrators, psychological impacts, and preventive measures needed to overcome this problem.",
     image: AnavaUGM,
@@ -49,8 +63,9 @@ export const datavisualization = [
     ],
   },
   {
-    id: 3,
-    title: "Infographics of Online Shopping Consumer Behavior Trends",
+    id: 4,
+    title:
+      "HMIF UMM - Infographics of Online Shopping Consumer Behavior Trends",
     description:
       "This project is an infographic work created in the infographic competition (SIC INDICATION) entered into by HMIF Muhammadiyah University of Malang to enliven the Informatics Engineering Department Anniversary event. This work presents an analysis of consumer behavior trends in online shopping by gender, including the top 3 purchase categories to the relationship between browsing activity and purchase frequency.",
     image: SIC_INDICATION,
@@ -63,8 +78,8 @@ export const datavisualization = [
     ],
   },
   {
-    id: 4,
-    title: "Tokopedei Transaction Performance Review Dashboard",
+    id: 5,
+    title: "MySkill - Tokopedei Transaction Performance Review Dashboard",
     description:
       "This is the Transaction Dashboard for Tokopedei, a dummy store created as part of the Fullstack Data Analysis Bootcamp assignment at MySkill. The dashboard was developed using Looker Studio and consists of two pages. It features visualizations of Sales & Profit Performance (All Time), Top Sales by Category, Top Sales by Payment Method, to Transaction Summary Table.",
     image: Tokopedei,
