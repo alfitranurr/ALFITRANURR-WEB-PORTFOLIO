f2b5afea80f36e4ab5bf808851c45bf1114ac4e3
alfitranurr <alfitranurr@gmail.com>
2025-06-17 09:10:15 +0700

    ui ux design mobile

--- frontend/src/database/Projects/BeyondData/mobiledevelopment.tsx
+++ frontend/src/database/Projects/BeyondData/mobiledevelopment.tsx
@@ -20,6 +20,10 @@ const mobiledevelopment = [
         text: "View Interactive Prototype",
         link: "https://www.figma.com/proto/hz3ncrMMSoE1bNIFPKepi9/Prak-mobile?node-id=84-29&p=f&t=FLu0e7MxfdCW0I6p-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=84%3A29",
       },
+      {
+        text: "View UI / UX Design",
+        link: "https://www.figma.com/design/hz3ncrMMSoE1bNIFPKepi9/Prak-mobile?node-id=0-1&t=RNycWEpMKCs1rWhP-1",
+      },
     ],
   },
 ];
