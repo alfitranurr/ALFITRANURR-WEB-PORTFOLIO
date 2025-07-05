266117158e7af31507718a60a3ecc6ac873310e0
alfitranurr <alfitranurr@gmail.com>
2025-07-05 09:16:17 +0700

    update cv

--- frontend/src/pages/about_me.tsx
+++ frontend/src/pages/about_me.tsx
@@ -22,7 +22,7 @@ const AboutMe = () => {
         {/* AL FITRA NUR RAMADHANI'S RESUME */}
         <div className="mt-6 mb-2 flex justify-center">
           <a
-            href="https://drive.google.com/file/d/1-dPEoIn-BdTtwohAiSreY5E3mH3_hB8H/view?usp=sharing"
+            href="https://drive.google.com/file/d/16tikotq7PnfBYyp_QqAt5pCohImntgYw/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="px-6 py-2 text-xs text-white font-semibold rounded-xl shadow-md text-center transition duration-300 bg-[var(--warna2-color)] border border-transparent hover:bg-white hover:text-[var(--warna2-color)] hover:border-[var(--warna2-color)]"
