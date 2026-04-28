83f655ccebaee8bd5b022235b93ba9be95fbbb75
Al Fitra Nur Ramadhani <alfitranurr@gmail.com>
2026-04-28 18:10:10 +0700

    .

--- frontend/src/pages/about_me.tsx
+++ frontend/src/pages/about_me.tsx
@@ -22,7 +22,7 @@ const AboutMe = () => {
         {/* AL FITRA NUR RAMADHANI'S RESUME */}
         <div className="mt-6 mb-2 flex justify-center">
           <a
-            href="https://drive.google.com/file/d/1w0nn8nJB_cPoUTnKcMmmQzR-gtTSgnM5/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Vsvs-r_GoxGEAanwEWqOFiGXUiqn_pkg/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="px-6 py-2 text-xs text-white font-semibold rounded-xl shadow-md text-center transition duration-300 bg-[var(--warna2-color)] border border-transparent hover:bg-white hover:text-[var(--warna2-color)] hover:border-[var(--warna2-color)]"
